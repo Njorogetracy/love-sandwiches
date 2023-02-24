693353d556657482768cf68765a9657e8d201e20
Hope Tracy <hnjoroge64@gmail.com>
2023-02-24 20:09:04 +0000

    Add requirements for deployement

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -24,7 +23,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
 
         sales_data = data_str.split(",")
         validate_data(sales_data)
@@ -53,26 +52,6 @@ def validate_data(values):
         return False
 
     return True
-
-
-# def update_sales_worksheet(data):
-#     """
-#     Updates sales worksheet, and add new row with the list data provided.
-#     """
-#     print("Updating sales worksheet...\n")
-#     sales_worksheet = SHEET.worksheet("sales")
-#     sales_worksheet.append_row(data)
-#     print("Sales worksheet updated successfully")
-
-
-# def update_surplus_worksheet(data):
-#     """
-#     Updates surplus worksheet, and add new row with the list data provided.
-#     """
-#     print("Updating surplus worksheet...\n")
-#     surplus_worksheet = SHEET.worksheet("surplus")
-#     surplus_worksheet.append_row(data)
-#     print("Surplus worksheet updated successfully")
 
 
 def update_worksheet(data, worksheet):
